444fc4d8dd687b54d58335c00886fa01b3656d89
eaknott <emknotty@gmail.com>
2024-03-28 12:32:39 +0800

    Add user stats

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,14 +4,16 @@ import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import GithubContext from '../context/github/GithubContext'
+import RepoList from '../components/repos/RepoList'
 
 function User() {
-    const { getUser, user, loading } = useContext(GithubContext)
+    const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext)
 
     const params = useParams()
 
     useEffect(() => {
         getUser(params.login)
+        getUserRepos(params.login)
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
@@ -157,8 +159,8 @@ function User() {
                         {public_gists}
                     </div>
                 </div>
-
             </div>
+            <RepoList repos={repos} />
         </div>
     </>
   )
